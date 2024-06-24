4b03cd164096261f517ee42406c90e5138a990cf
Mori <6064038+MortezaMirmojarabian@users.noreply.github.com>
2024-06-24 17:08:04 +0330

    Update posts.js

--- posts.js
+++ posts.js
@@ -1,8 +1,9 @@
+function toggleNotification() {
+  document.getElementById('notification').toggleAttribute('hidden');
+}
+
 document.querySelector('main footer button').addEventListener('click', function() {
   navigator.clipboard.writeText(location);
-  function toggleNotification() {
-    document.getElementById('notification').toggleAttribute('hidden');
-  }
   toggleNotification();
-  setTimeout(toggleNotification, 1500);
+  setTimeout(toggleNotification, 2000);
 });
